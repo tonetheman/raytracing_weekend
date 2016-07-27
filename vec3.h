f79c75e6e45cd33374737324eb32a4ceedd008af
tony <tonetheman@gmail.com>
2016-07-27 13:44:01 -0500

    added more to header file

--- vec3.h
+++ vec3.h
@@ -1,6 +1,7 @@
 #ifndef VEC3
 #define VEC3
 
+#include <cmath>
 
 class vec3 {
 public:
@@ -12,16 +13,32 @@ public:
 		e[2] = e2;
 	}
 	inline float x() const { return e[0]; }
-
-
+	inline float y() const { return e[1]; }
+	inline float z() const { return e[2]; }
+	inline float r() const { return e[0]; }
+	inline float g() const { return e[1]; }
+	inline float b() const { return e[2]; }
+	
+	inline const vec3& operator+() const { return *this; }
+	inline vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
 	inline float operator[](int i) const { return e[i]; }
 	inline float& operator[](int i) { return e[i]; }
 
-	// unary plus
-	inline const vec3& operator+() const { return *this; }
-
-	// scalar mult
+	inline vec3& operator+=(const vec3 &v2);
+	inline vec3& operator-=(const vec3 &v2);
+	inline vec3& operator*=(const vec3 &v2);
+	inline vec3& operator/=(const vec3 &v2);
 	inline vec3& operator*=(const float t);
+	inline vec3& operator/=(const float t);
+
+	inline float length() const {
+		return sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
+	}
+	inline float squared_length() const {
+		return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+	}
+	inline void make_unit_vector();
+
 };
 
 #endif
