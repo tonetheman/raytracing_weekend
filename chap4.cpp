7696ac6abc483816643d41c955de172b7d78b0b5
tony <tonetheman@gmail.com>
2016-07-30 08:22:01 -0500

    chap4 working

--- chap4.cpp
+++ chap4.cpp
@@ -10,11 +10,16 @@
 bool hit_sphere(const vec3& center, float radius, const ray& r) {
 	vec3 oc = r.origin() - center;
 	float a = dot(r.direction(), r.direction());
+	float b = 2.0 * dot(oc, r.direction());
+	float c = dot(oc,oc) - radius*radius;
+	float discriminant = b*b - 4 * a * c;
+	return (discriminant > 0);
 }
 
 
 vec3 color(const ray& r) {
 	if (hit_sphere(vec3(0,0,-1), 0.5, r)) {
+		return vec3(1,0,0);
 	}
 	vec3 unit_direction = unit_vector(r.direction());
 	float t = 0.5 * (unit_direction.y() + 1.0);
@@ -52,7 +57,7 @@ int main() {
 			buffer[rp+2] = ib;
 		}
 
-		stbi_write_png("out5.png", 200, 200, STRIDE, buffer, 200*STRIDE);
+		stbi_write_png("out_chap4.png", 200, 200, STRIDE, buffer, 200*STRIDE);
 	}	
 
 	return 0;
